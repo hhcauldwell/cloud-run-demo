f43bc1d0124e64ce582fd139b00e1101a61b6836
Hannah Cauldwell <hhcauldwell@gmail.com>
2022-01-13 19:22:07 -0800

    Adding config

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    name = os.environ.get("NAME", "World 7")
+    name = os.environ.get("NAME", "World")
     return "Hello {}!".format(name)
 
 
